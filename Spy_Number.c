47432871f4b0bb5ceae12abff399c0f1881a75ae
21MH1A05C2 <97426521+21MH1A05C2@users.noreply.github.com>
2022-06-29 20:27:43 +0530

    #include<stdio.h> int main() {     int n,r,sum=0,f=1;     scanf("%d",&n);     while(n!=0)     {         r=n%10;         sum=sum+r;         f=f*r;         n=n/10;     }     if(sum==f)     {         printf("Spy Number");     }     else     {         printf("Not Spy Number");     } }

--- Spy_Number.c
+++ Spy_Number.c
@@ -1,17 +1,16 @@
-
 #include<stdio.h>
 int main()
 {
-    int n,r,m=1,s=0;
+    int n,r,sum=0,f=1;
     scanf("%d",&n);
-    while(n>0)
+    while(n!=0)
     {
         r=n%10;
-        s=s+r;
-        m=m*r;
+        sum=sum+r;
+        f=f*r;
         n=n/10;
     }
-    if(s==m)
+    if(sum==f)
     {
         printf("Spy Number");
     }
